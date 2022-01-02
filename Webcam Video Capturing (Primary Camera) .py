06b219c326b5e9c69d8adb149ffa9579e19e4323
Hemant Yadav <78432613+Hemant051@users.noreply.github.com>
2022-01-02 19:46:59 +0530

    Add files via upload

---
+++ Webcam Video Capturing (Primary Camera) .py
@@ -0,0 +1,30 @@
+# Webcam Frames Capturing
+import cv2
+import numpy as np
+
+#seting the frame ratio
+frameWidth = 640
+frameHeight = 480
+
+#Command to capture video from primary webcam
+vid = cv2.VideoCapture(0)
+# to set the width of the frame
+vid.set(3,640)
+# to set the height of the frame
+vid.set(4,480)
+# to set the brightness of the frame
+vid.set(10,150)
+
+# Applying the loop to capture the read the frames of video
+while True:
+    # check is the variable for checking the boolean values and frame is another variable for storing the readed frames from the video
+    check,frame = vid.read()
+
+    #showing the frames captured
+    cv2.imshow("webcam video", frame)
+    # conditional statement for stoping the frames display
+    if cv2.waitKey(1) & 0XFF == ord("q"):
+        break
+# Terminating commands
+vid.release()
+cv2.destroyAllWindows()
